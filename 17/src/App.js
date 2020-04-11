3a7f53bf21987598a11841f13d234a26b054bac9
KeraKim <kerakim0716@gmail.com>
2020-04-11 17:37:55 +0900

    17장 초기화

--- 17/src/App.js
+++ 17/src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
-import Counter from "./components/Counter";
-import Todos from "./components/Todos";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Counter number={0} />
-      <hr />
-      <Todos />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
