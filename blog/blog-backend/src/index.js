b3992289e4c24ba21bfe3693fe679afe6a4b8523
KeraKim <kerakim0716@gmail.com>
2020-05-26 06:28:27 +0900

    no message

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,8 +1,18 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose')
 
 const api = require('./api');
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false}).then(() => {
+  console.log('Connected to MongoDB')
+}).catch(e => {
+  console.log(e)
+})
 
 const app = new Koa();
 const router = new Router();
@@ -13,6 +23,7 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Lostening to port 4000');
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
 });
